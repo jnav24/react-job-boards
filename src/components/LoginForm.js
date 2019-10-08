ea64988854951ac2737a73f28af7fb3fd51fdc1d
Justin Navarro <info@justinnavarro.net>
2019-10-08 12:22:44 -0400

    build out the login form

--- src/components/LoginForm.js
+++ src/components/LoginForm.js
@@ -1,10 +1,67 @@
 import React, { Component } from 'react';
+import { login } from '../helpers/auth';
 
 export default class LoginForm extends Component {
+    state = {
+        email: '',
+        error: false,
+        password: '',
+    };
+
+    handleChange = (e) => {
+        e.preventDefault();
+        const {name, value} = e.target;
+        this.setState({ [name]: value });
+    };
+
+    handleClick = async (e) => {
+        e.preventDefault();
+        const response = await login(this.state.email, this.state.password);
+
+        if (response) {
+            this.props.onLogin();
+        } else {
+            this.setState({ error: true });
+        }
+    };
+
     render() {
         return (
             <form>
-                login form
+                <div className="field">
+                    <label className="label">Email</label>
+                    <div className="control">
+                        <input
+                            className="input"
+                            name="email"
+                            onChange={this.handleChange}
+                            type="text"
+                            value={this.state.email} />
+                    </div>
+                </div>
+
+                <div className="field">
+                    <label className="label">Password</label>
+                    <div className="control">
+                        <input
+                            className="input"
+                            name="password"
+                            onChange={this.handleChange}
+                            type="password"
+                            value={this.state.password} />
+                    </div>
+                </div>
+
+                <div className="field">
+                    <p className="help is-danger">{this.state.error && 'Invalid credentials'}</p>
+                    <div className="control">
+                        <button
+                            className="button is-link"
+                            onClick={this.handleClick}>
+                            Login
+                        </button>
+                    </div>
+                </div>
             </form>
         );
     }
