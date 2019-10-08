6763ad5de0b1df117d3860739aff6db630a16c1e
Justin Navarro <info@justinnavarro.net>
2019-10-08 12:29:03 -0400

    add try/catch in case of server errors

--- src/helpers/auth.js
+++ src/helpers/auth.js
@@ -4,16 +4,20 @@ const serverUrl = 'http://localhost:9000';
 export const isLoggedIn = () => !!localStorage.getItem(accessTokenKey);
 
 export const login = async (email, password) => {
-    const response = await fetch(serverUrl, {
-        method: 'POST',
-        headers: {'content-type': 'application/json'},
-        body: JSON.stringify({ email, password }),
-    });
+    try {
+        const response = await fetch(serverUrl, {
+            method: 'POST',
+            headers: {'content-type': 'application/json'},
+            body: JSON.stringify({ email, password }),
+        });
 
-    if (response.ok) {
-        const {token} = await response.json();
-        localStorage.setItem(accessTokenKey, token);
+        if (response.ok) {
+            const {token} = await response.json();
+            localStorage.setItem(accessTokenKey, token);
+        }
+
+        return response.ok;
+    } catch (error) {
+        return false;
     }
-
-    return response.ok;
 };
