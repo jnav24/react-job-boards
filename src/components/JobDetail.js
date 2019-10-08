35ea1a87bf27d37d5843518a9d27c6d7bcf37367
Justin Navarro <info@justinnavarro.net>
2019-10-08 14:59:22 -0400

    set temp job data in job detail component

--- src/components/JobDetail.js
+++ src/components/JobDetail.js
@@ -4,7 +4,31 @@ export default class JobDetail extends Component {
     jobId = () => this.props.match.params;
 
     state = {
-        job: jobs.find(job => job.id === this.jobId()),
+        job: {
+            id: '',
+            title: '',
+            company: {
+                id: '',
+                name: '',
+                description: '',
+            },
+            description: '',
+        },
+    };
+
+    componentDidMount = () => {
+        this.setState({
+            job: {
+                id: 'job1',
+                title: 'Job 1',
+                company: {
+                    id: 'company1',
+                    name: 'Company A',
+                    description: 'Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.'
+                },
+                description: 'Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.'
+            }
+        });
     };
 
     render() {
