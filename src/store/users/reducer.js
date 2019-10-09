52f06c8439bdb494dcc6084c13b7c80e498b34cb
Justin Navarro <info@justinnavarro.net>
2019-10-09 18:22:54 -0400

    created users store

---
+++ src/store/users/reducer.js
@@ -0,0 +1,24 @@
+const getDefaultState = () => {
+    return {
+        email: '',
+        company: {
+            name: '',
+            description: '',
+        },
+    };
+};
+
+const initialState = getDefaultState();
+
+const usersReducer = (state = initialState, action) => {
+    switch (action.type) {
+        case 'ADD_USER':
+            return { ...state, ...action.payload };
+        case 'REMOVE_USER':
+            return getDefaultState();
+        default:
+            return state;
+    }
+};
+
+export default usersReducer;
