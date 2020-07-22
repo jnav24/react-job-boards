b0385e39e4e4478e6a9ef5bb35f1f162d977262a
Justin Navarro <info@justinnavarro.net>
2020-07-21 23:12:01 -0400

    list company's jobs in the company detail page

--- src/components/CompanyDetail.js
+++ src/components/CompanyDetail.js
@@ -1,11 +1,13 @@
 import React, { Component } from 'react';
 import { loadCompany } from '../helpers/requests';
+import JobList from './JobList';
 
 export default class CompanyDetail extends Component {
     state = {
         company: {
             description: '',
             name: '',
+            jobs: [],
         },
     };
 
@@ -21,6 +23,7 @@ export default class CompanyDetail extends Component {
             <div>
                 <h1 className="title">{this.state.company.name}</h1>
                 <div className="box">{this.state.company.description}</div>
+                <JobList jobs={this.state.company.jobs} />
             </div>
         );
     }
--- src/helpers/requests.js
+++ src/helpers/requests.js
@@ -28,6 +28,10 @@ export async function loadCompany(id) {
             id
             name
             description
+            jobs {
+                id
+                title
+            }
         }
     }`;
     const { company } = await graphqlRequest(query, { id });
